ceabb1d85bff4110274d23a06b2fb436efd31d3e
fsrocha-dev <contato.frankrocha@gmail.com>
2020-01-16 12:10:02 -0300

    register user and axios install

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,34 @@
 const { Router } = require("express");
+const axios = require("axios");
+const User = require("./models/User");
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  return res.send("Inicio");
+//Cadastrar novo usuário
+routes.post("/users", async (req, res) => {
+  try {
+    const { github_user, skills } = req.body;
+
+    const response = await axios.get(
+      `https://api.github.com/users/${github_user}`
+    );
+
+    const { name = login, avatar_url, bio } = response.data;
+
+    const skillsArray = skills.split(",").map(skill => skill.trim());
+
+    const user = await User.create({
+      github_user,
+      name,
+      avatar: avatar_url,
+      bio,
+      skills: skillsArray
+    });
+
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = routes;
