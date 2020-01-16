552c45bf0a7bfbb6f6a819110892555797b3169f
fsrocha-dev <contato.frankrocha@gmail.com>
2020-01-16 10:39:12 -0300

    instalando mongoose e configurando db

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
+
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send("Inicio");
