b19dc41d48119dc7482a628b1da829d7bbcaafb3
fsrocha-dev <contato.frankrocha@gmail.com>
2020-01-16 12:38:46 -0300

    creating user controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const User = require("../models/User");
+
+module.exports = {
+  async store(req, res) {
+    try {
+      const { github_user, skills, lat, long } = req.body;
+
+      const response = await axios.get(
+        `https://api.github.com/users/${github_user}`
+      );
+
+      const { name = login, avatar_url, bio } = response.data;
+
+      const skillsArray = skills.split(",").map(skill => skill.trim());
+
+      const location = {
+        type: "Point",
+        coordinates: [long, lat]
+      };
+
+      const user = await User.create({
+        github_user,
+        name,
+        avatar: avatar_url,
+        bio,
+        skills: skillsArray,
+        location
+      });
+
+      return res.json(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
